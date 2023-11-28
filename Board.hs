5ff21fa0a0c29d328022f39dfe59472e247b0849
jmore3 <84981020+jmore3@users.noreply.github.com>
2023-11-28 13:10:51 -0700

    Create Board.hs

---
+++ Board.hs
@@ -0,0 +1,77 @@
+-- Board.hs
+module Board where
+
+-- Define types for the Board, Players, and Stones
+type Board = [[Maybe Player]]
+type Player = Int
+type Stone = Maybe Player
+
+-- Constants for the players
+mkPlayer :: Player
+mkPlayer = 1
+
+mkOpponent :: Player
+mkOpponent = 2
+
+-- Create an empty board of size n x n
+mkBoard :: Int -> Board
+mkBoard n = replicate n (replicate n Nothing)
+
+-- Return the size of the board
+size :: Board -> Int
+size = length
+
+-- Return a row of the board
+row :: Int -> Board -> [Stone]
+row y bd = bd !! (y - 1)
+
+-- Return a column of the board
+column :: Int -> Board -> [Stone]
+column x bd = map (!! (x - 1)) bd
+
+-- Mark a place on the board by a player
+mark :: Int -> Int -> Board -> Player -> Board
+mark x y bd p = 
+    take (y - 1) bd ++ 
+    [replace (bd !! (y - 1)) x p] ++ 
+    drop y bd
+  where
+    replace :: [Stone] -> Int -> Player -> [Stone]
+    replace row x p = take (x - 1) row ++ [Just p] ++ drop x row
+
+-- Check if a place on the board is empty
+isEmpty :: Int -> Int -> Board -> Bool
+isEmpty x y bd = (bd !! (y - 1)) !! (x - 1) == Nothing
+
+-- Check if a place on the board is marked
+isMarked :: Int -> Int -> Board -> Bool
+isMarked x y bd = not $ isEmpty x y bd
+
+-- Check if a place on the board is marked by a specific player
+isMarkedBy :: Int -> Int -> Board -> Player -> Bool
+isMarkedBy x y bd p = (bd !! (y - 1)) !! (x - 1) == Just p
+
+-- Return the player who marked a specific cell
+marker :: Int -> Int -> Board -> Maybe Player
+marker x y bd = (bd !! (y - 1)) !! (x - 1)
+
+-- Check if the board is full
+isFull :: Board -> Bool
+isFull bd = all (all isJust) bd
+
+-- Check if the game is won by a player
+isWonBy :: Board -> Player -> Bool
+isWonBy bd p = undefined  -- Implement the winning logic
+
+-- Check if the game is a draw
+isDraw :: Board -> Bool
+isDraw bd = isFull bd && not (isWonBy bd mkPlayer || isWonBy bd mkOpponent)
+
+-- Check if the game is over
+isGameOver :: Board -> Bool
+isGameOver bd = isDraw bd || isWonBy bd mkPlayer || isWonBy bd mkOpponent
+
+-- Convert the board to a string for printing
+boardToStr :: (Player -> Char) -> Board -> String
+boardToStr playerToChar bd = undefined  -- Implement string representation logic
+
