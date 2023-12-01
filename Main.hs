8155ce55c1b4f3856aef925084af30fd0fd46d43
jmore3 <84981020+jmore3@users.noreply.github.com>
2023-12-01 11:46:22 -0700

    Update Main.hs

--- Main.hs
+++ Main.hs
@@ -22,7 +22,7 @@ playGame board player = do
         then putStrLn "Game over!" >> announceResult board
         else do
             -- Read the next move
-            putStrLn $ "Player " ++ show player ++ "'s turn."
+            putStrLn $ "Player " ++ show player ++ "'s turn (Enter x y coordinates):"
             move <- readXY board player
             let newBoard = mark (fst move) (snd move) board player
 
@@ -39,8 +39,18 @@ playerToChar p
 -- Read a pair of indices (x, y) from the user
 readXY :: Board -> Player -> IO (Int, Int)
 readXY board player = do
-    -- Implement logic to read and validate user input
-    undefined
+    input <- getLine
+    let parsed = reads input :: [(Int, Int)]
+    if null parsed
+       then putStrLn "Invalid input. Please enter x and y coordinates." >> readXY board player
+       else let (x, y) = head parsed
+            in if isValidMove x y board
+               then return (x, y)
+               else putStrLn "Invalid move. Please try again." >> readXY board player
+
+isValidMove :: Int -> Int -> Board -> Bool
+isValidMove x y board = 
+    x > 0 && x <= size board && y > 0 && y <= size board && isEmpty x y board
 
 -- Announce the result of the game
 announceResult :: Board -> IO ()
@@ -48,4 +58,3 @@ announceResult board
     | isWonBy board mkPlayer = putStrLn "Player 1 wins!"
     | isWonBy board mkOpponent = putStrLn "Player 2 wins!"
     | otherwise = putStrLn "It's a draw!"
-
